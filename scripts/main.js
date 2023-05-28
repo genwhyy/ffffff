d7044694a249348e9f57473c014b5a52e2f8aada
Alissa <75789938+genwhyy@users.noreply.github.com>
2023-05-28 13:07:54 +0600

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,11 @@
-window.addEventListener('keydown', function(e){
-const audioKey = document.querySelector('audio[data-key="${e.keyCode}"]');
-console.log(audioKey);
-});
+window.addEventListener('keydown', playNoteKeyboard);
+
+function playNoteKeyboard(e) {
+    if (e.repeat) return;
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if (!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add('key-active');
+    }
