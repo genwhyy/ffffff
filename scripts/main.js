b17ff634ec1107195f6e1e9fa89e2ef8b7a0837a
Alissa <75789938+genwhyy@users.noreply.github.com>
2023-05-28 17:42:27 +0600

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,8 @@
 window.addEventListener('keydown', playNoteKeyboard);
-
+window.addEventListener('keyup', stopNoteKeyboard);
+//keyboard
 function playNoteKeyboard(e) {
-    if (e.repeat) return;
+    if (e.repeat) return; //если клавиша залипает чтобы не убивались уши 
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     if (!audio) return;
@@ -9,3 +10,44 @@ function playNoteKeyboard(e) {
     audio.play();
     key.classList.add('key-active');
     }
+function stopNoteKeyboard(e) {
+    let key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if (!key) return;
+    key.classList.remove('key-active');
+};
+// mouse 
+/*
+let isMouseDown = false;
+window.addEventListener('mousedown', playNoteMousedown, false);
+window.addEventListener('mouseup', stopNoteMousedown);
+window.addEventListener('mouseover', playNoteMouseover);
+window.addEventListener('mouseout', stopNoteMouseover);
+
+function playNoteMousedown(e) {
+    if (!e.target.classList.contains('key')) return;
+    const audio1 = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    e.target.classList.add('key-active');
+    audio.currentTime = 0;
+    audio1.play();
+    isMouseDown = true;
+};
+
+function stopNoteMousedown(e) {
+    e.target.classList.remove('key-active');
+    e.target.removeEventListener('mouseover', playNoteMouseover);
+    isMouseDown = false;
+};
+
+function playNoteMouseover(e) {
+    if (!isMouseDown) return;
+    if (!e.target.classList.contains('key')) return;
+    const audio1 = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    e.target.classList.add('key-active');
+    audio1.currentTime = 0;
+    audio.play();
+};
+
+function stopNoteMouseover(e) {
+    e.target.classList.remove('key-active');
+    e.target.removeEventListener('mouseover', playNoteMouseover);
+};
